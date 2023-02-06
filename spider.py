aac1ffec032bb924deb1cb1f2d11a5640d95b4b8
Anmar <anmarabdi@yahoo.com>
2023-02-06 00:04:17 -0500

    working scraper

--- spider.py
+++ spider.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, redirect, render_template
 from bs4 import BeautifulSoup
+import requests # To scrape HTML from page
+
 
 app = Flask(__name__)
 
@@ -8,9 +10,23 @@ app = Flask(__name__)
 def create_playlist():
     if request.method == "POST":
         aplink = request.form.get('apple_music_playlist_url')
-        return "Your name is " + aplink
+        source = requests.get(aplink).text
+        soup = BeautifulSoup(source, 'html.parser')
+        for title in soup.find_all('div', 'songs-list-row__song-name'):
+            isotitles = (format(title.text)) #messed up
+            
+        for artist in soup.find_all('div', 'songs-list-row__by-line'):
+            isoartist = (format(artist.text))
+            print(isotitles + isoartist)
+    
+
+    
+    
     return render_template('index.html', link='apple_music_playlist_url')
     
+
+
+
 
 
 if __name__ == "__main__":
