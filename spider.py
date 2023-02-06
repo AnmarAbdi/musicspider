01884270722ad87db098ba48883710960f16e2b0
Anmar <anmarabdi@yahoo.com>
2023-02-05 19:41:52 -0500

    Test form with flask

--- spider.py
+++ spider.py
@@ -1,37 +1,15 @@
-import requests
 from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route("/index.html")
-def index():
-    return render_template("index.html")
 
-@app.route("/create_playlist", methods=["POST"])
+@app.route('/', methods=['GET', 'POST'])
 def create_playlist():
-    # Replace with your actual access token
-    access_token = "your_access_token"
-
-    # Replace with the ID of the user you want to create the playlist for
-    user_id = "user_id"
-
-    # Replace with the URL of the Apple Music playlist
-    apple_music_playlist_url = request.form["apple_music_playlist_url"]
-
-    # Retrieve the tracks from the Apple Music playlist
-    # ...
-
-    # Define the playlist details
-    playlist_name = "My Python Playlist"
-    playlist_description = "A playlist created using the Spotify Web API and Python"
-
-    # Create the new playlist
-    # ...
-
-    # Add the tracks to the playlist
-    # ...
-
-    return redirect("/")
+    if request.method == "POST":
+        aplink = request.form.get('apple_music_playlist_url')
+        return "Your name is " + aplink
+    return render_template('index.html', link='apple_music_playlist_url')
+    
 
 
 if __name__ == "__main__":
