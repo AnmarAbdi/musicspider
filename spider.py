2ed7f6fdc24994749f74cb5b0397e640bd5397c7
Anmar <anmarabdi@yahoo.com>
2023-08-04 10:39:48 -0400

    stuck

--- spider.py
+++ spider.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, redirect, render_template, session
+from flask import Flask, request, redirect, render_template, session, jsonify
 from bs4 import BeautifulSoup
 import requests # To scrape HTML from page
 from dotenv import load_dotenv
 import os
 import json
 import mysql.connector
+import time
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -24,11 +25,46 @@ TOKEN_URL = "https://accounts.spotify.com/api/token"
 REDIRECT_URI = "http://127.0.0.1:5000/callback"
 SCOPES = "playlist-modify-public playlist-modify-private"
 
+def refresh_access_token_if_needed():
+    access_token = session.get('access_token')
+    refresh_token = session.get('refresh_token')
+    expires_at = session.get('expires_at')
+
+    # If the access token has expired, refresh it
+    if int(time.time()) > expires_at:
+        # Use the refresh token to get a new access token
+        data = {
+            'grant_type': 'refresh_token',
+            'refresh_token': refresh_token,
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        }
+        response = requests.post(TOKEN_URL, data=data)
+        token_info = response.json()
+        new_access_token = token_info['access_token']
+        new_expires_in = token_info['expires_in']
+
+        # Store the new access_token and expiration time in the session
+        session['access_token'] = new_access_token
+        session['expires_at'] = int(time.time()) + new_expires_in
+
+        return new_access_token
+
+    return access_token
+
+
 
 @app.route('/')
 def index():
-    print("TESTTT================")
-    return render_template('index.html')
+    user_id = session.get('user_id')
+    is_authenticated = False if user_id is None else True
+    return render_template('index.html', is_authenticated=is_authenticated)
+
+
+@app.route('/get_spotify_login_url', methods=['GET'])
+def get_spotify_login_url():
+    auth_url = f"{AUTH_URL}?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={SCOPES}"
+    return jsonify({'url': auth_url})
 
 @app.route('/', methods=['POST'])
 def parse_playlist():
@@ -77,14 +113,15 @@ def parse_playlist():
 
         cursor.close()
         cnx.close()
-        return ":D"
+        return redirect('/create_playlist')
         
         
 
 @app.route('/create_playlist', methods=['GET', 'POST'])
 def create_playlist():
-    access_token = session.get('access_token')
+    access_token = refresh_access_token_if_needed()
     if not access_token:
+        # If user not logged in, redirect to Spotify login page
         auth_url = f"{AUTH_URL}?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={SCOPES}"
         return redirect(auth_url)
 
@@ -94,71 +131,98 @@ def create_playlist():
 
     user_id = session.get('user_id')
     if not user_id:
-        # The same logic as before...
-        # Get the user_id and access_token
+        user_info_response = requests.get("https://api.spotify.com/v1/me", headers=headers)
+        if user_info_response.status_code == 401:
+            # Access token expired, refresh token
+            refresh_token = session.get('refresh_token')
+            if not refresh_token:
+                return "Refresh token is missing."
+
+            refresh_data = {
+                'grant_type': 'refresh_token',
+                'refresh_token': refresh_token,
+                'client_id': CLIENT_ID,
+                'client_secret': CLIENT_SECRET
+            }
+            refresh_response = requests.post(TOKEN_URL, data=refresh_data)
+            refresh_response.raise_for_status()
+            refresh_token_info = refresh_response.json()
+            access_token = refresh_token_info['access_token']
+            session['access_token'] = access_token
+            headers['Authorization'] = f"Bearer {access_token}"
+
+            # Retry getting user info
+            user_info_response = requests.get("https://api.spotify.com/v1/me", headers=headers)
+
+        user_info_response.raise_for_status()
+        user_info = user_info_response.json()
+        user_id = user_info.get('id')
+        session['user_id'] = user_id
 
     if not user_id:
         return "User ID is missing."
 
     # Create a new playlist
     create_playlist_data = {
-        "name": "it worked>:D",
+        "name": "Apple Music to Spotify Playlist",
         "public": True
     }
-    response = requests.post(
-        f"https://api.spotify.com/v1/users/{session.get('user_id')}/playlists", 
-        headers={
-            "Authorization": f"Bearer {access_token}",
-            "Content-Type": "application/json"
-        },
-        data=json.dumps(create_playlist_data)
-    )
-    response.raise_for_status()
+    response = requests.post(f"https://api.spotify.com/v1/users/{user_id}/playlists", headers=headers, data=json.dumps(create_playlist_data))
+    response.raise_for_status()  # This will raise an exception if the request failed
     playlist_id = response.json()['id']
 
-    # Now add songs to the playlist
+    # Connect to database
+    with mysql.connector.connect(user=os.getenv('DB_USER'), password=os.getenv('DB_PASS'), host=os.getenv('DB_HOST'), database=os.getenv('DB_NAME')) as cnx:
+        with cnx.cursor() as cursor:
+            cursor.execute("SELECT title, artist FROM songs")
+            songs = cursor.fetchall()
+
+    # Add songs to the playlist
     song_uris = []
-
-    # Connect to the database
-    cnx = mysql.connector.connect(
-        user=os.getenv('DB_USER'), 
-        password=os.getenv('DB_PASS'), 
-        host=os.getenv('DB_HOST'), 
-        database=os.getenv('DB_NAME')
-    )
-
-    # Fetch songs from the database
-    with cnx.cursor() as cursor:
-        cursor.execute("SELECT title, artist FROM songs")
-        songs = cursor.fetchall()
-
     for song_title, artist in songs:
-        response = requests.get(
-            f"https://api.spotify.com/v1/search?q=track:{song_title}%20artist:{artist}&type=track&limit=1",
-            headers=headers
-        )
+        response = requests.get(f"https://api.spotify.com/v1/search?q=track:{song_title}%20artist:{artist}&type=track&limit=1", headers=headers)
         response.raise_for_status()
         tracks = response.json()['tracks']['items']
         if tracks:
             song_uris.append(tracks[0]['uri'])
 
-    # Then, add these songs to the playlist
-    add_songs_data = {
-        "uris": song_uris
-    }
-    response = requests.post(
-        f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks",
-        headers={
-            "Authorization": f"Bearer {access_token}",
-            "Content-Type": "application/json"
-        },
-        data=json.dumps(add_songs_data)
-    )
+    add_songs_data = {"uris": song_uris}
+    response = requests.post(f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks", headers=headers, data=json.dumps(add_songs_data))
     response.raise_for_status()
 
-    cnx.close()
-
     return render_template('index.html', link='apple_music_playlist_url')
+
+@app.route('/callback')
+def callback():
+    # The user is redirected back to your app with the authorization code
+    code = request.args.get('code')
+
+    # Request access and refresh tokens
+    auth = (CLIENT_ID, CLIENT_SECRET)
+    data = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': REDIRECT_URI
+    }
+    response = requests.post(TOKEN_URL, auth=auth, data=data)
+    token_info = response.json()
+    access_token = token_info['access_token']
+    refresh_token = token_info['refresh_token']
+
+    # Store the access and refresh tokens in session
+    session['access_token'] = access_token
+    session['refresh_token'] = refresh_token
+
+    # Get user ID from Spotify API and store in session
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    user_info_response = requests.get("https://api.spotify.com/v1/me", headers=headers)
+    user_info = user_info_response.json()
+    user_id = user_info.get('id')
+    session['user_id'] = user_id
+
+    return redirect('/create_playlist')
 
 
 if __name__ == "__main__":
